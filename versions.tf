28a1a646768ac971fba4f63767c735acc7952f3b
bsvihovec <bsvihovec@yahoo.com>
2021-07-26 13:58:41 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ibm = {
       source = "jonwoodlief/catalog"
-      version = "0.0.100"
+      version = "0.0.200"
       #source = "IBM-Cloud/ibm"
       #version = "1.27.1"
     }
