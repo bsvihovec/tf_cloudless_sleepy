5ea229852038ec50791b71bdb55c704b07f5992f
bsvihovec <bsvihovec@yahoo.com>
2021-09-07 11:13:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,25 @@
-resource "ibm_cm_catalog" "cm_catalog" {
-    label = "tf_test_offering_catalog"
-    short_description = "testing terraform provider with catalog"
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello, I am a template. My sample_var value = $${sample_var}"
+
+  vars = {
+    sample_var = var.sample_var
+  }
 }
-resource "ibm_cm_offering" "cm_offering1" {
-    catalog_id = ibm_cm_catalog.cm_catalog.id
-    label = "tf_test_offering"
-    tags = var.string_array_with_type
-    test_int = var.int_with_type
-    test_float = var.float_with_type
-    test_bool = var.boolean_with_type
-    test_map = var.object_with_type
-}
+
+# resource "ibm_cm_catalog" "cm_catalog" {
+#     label = "tf_test_offering_catalog"
+#     short_description = "testing terraform provider with catalog"
+# }
+# resource "ibm_cm_offering" "cm_offering1" {
+#     catalog_id = ibm_cm_catalog.cm_catalog.id
+#     label = "tf_test_offering"
+#     tags = var.string_array_with_type
+#     test_int = var.int_with_type
+#     test_float = var.float_with_type
+#     test_bool = var.boolean_with_type
+#     test_map = var.object_with_type
+# }
 # resource "ibm_cm_offering" "cm_offering2" {
 #     catalog_id = ibm_cm_catalog.cm_catalog.id
 #     label = "tf_test_offering2"
