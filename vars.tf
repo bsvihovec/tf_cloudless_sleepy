8cddbbf2f05c7e9ba181f143e4c76e29b26397d4
bsvihovec <bsvihovec@yahoo.com>
2021-09-07 11:23:49 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,111 +10,111 @@ variable "sample_var" {
   default     = "hello"
 }
 
-variable "sleepy_time" {
-  description = "How long our local-exec will take a nap."
-  default     = 0
-}
-
-variable "string_array_with_type" {
-  type = list(string)
-}
-
-variable "boolean_with_type" {
-  type = bool
-}
-
-variable "int_with_type" {
-  type = number
-}
-
-variable "float_with_type" {
-  type = number
-}
-
-# variable "object_array_with_type" {
-#   type = list(object({
-#               foo = string
-#   }))
+# variable "sleepy_time" {
+#   description = "How long our local-exec will take a nap."
+#   default     = 0
 # }
 
-variable "object_with_type" {
-  type = object({foo = string})
-  default = {foo:"bar"}
-}
-
-variable "string_array_no_type" {
-  default = ["a", "b", "c"] 
-}
-
-variable "boolean_no_type" {
-  default = false
-}
-
-variable "int_no_type" {
-  default = 1
-}
-
-variable "float_no_type" {
-  default = 1.1
-}
-
-# variable "object_array_no_type" {
-#  default = [{"foo":"bar"}, {"foo":"baz"}] 
-# }
-
-# variable "object_no_type" {
-#   default = {"foo":"bar"}
-# }
-
-variable "string_array_with_type_and_default_1" {
-  type = list(string)
-  default = ["a", "b", "c"]
-}
-
-# variable "string_array_with_type_and_default_2" {
+# variable "string_array_with_type" {
 #   type = list(string)
-#   default = [
-#     "a", 
-#     "b", 
-#     "c"
-#   ]
 # }
 
-variable "boolean_with_type_and_default" {
-  type = bool
-  default = true
-}
-
-variable "int_with_type_and_default" {
-  type = number
-  default = 200
-}
-
-variable "float_with_type_and_default" {
-  type = number
-  default = 200.100
-}
-
-# variable "object_array_with_type_and_default_1" {
-#   type = list(object({
-#               foo = string
-#   }))
-#   default = [{"foo"="bar"}]
+# variable "boolean_with_type" {
+#   type = bool
 # }
 
-# variable "object_array_with_type_and_default_2" {
-#   type = list(object({
-#               foo = string
-#   }))
-#   default = [
-#     {foo="bar"}
-#   ]
+# variable "int_with_type" {
+#   type = number
 # }
 
-variable "object_with_type_and_default_1" {
-  type = object({foo = string})
-  default = {foo:"bar"}
-}
+# variable "float_with_type" {
+#   type = number
+# }
+
+# # variable "object_array_with_type" {
+# #   type = list(object({
+# #               foo = string
+# #   }))
+# # }
+
+# variable "object_with_type" {
+#   type = object({foo = string})
+#   default = {foo:"bar"}
+# }
+
+# variable "string_array_no_type" {
+#   default = ["a", "b", "c"] 
+# }
+
+# variable "boolean_no_type" {
+#   default = false
+# }
+
+# variable "int_no_type" {
+#   default = 1
+# }
+
+# variable "float_no_type" {
+#   default = 1.1
+# }
+
+# # variable "object_array_no_type" {
+# #  default = [{"foo":"bar"}, {"foo":"baz"}] 
+# # }
+
+# # variable "object_no_type" {
+# #   default = {"foo":"bar"}
+# # }
+
+# variable "string_array_with_type_and_default_1" {
+#   type = list(string)
+#   default = ["a", "b", "c"]
+# }
+
+# # variable "string_array_with_type_and_default_2" {
+# #   type = list(string)
+# #   default = [
+# #     "a", 
+# #     "b", 
+# #     "c"
+# #   ]
+# # }
+
+# variable "boolean_with_type_and_default" {
+#   type = bool
+#   default = true
+# }
+
+# variable "int_with_type_and_default" {
+#   type = number
+#   default = 200
+# }
+
+# variable "float_with_type_and_default" {
+#   type = number
+#   default = 200.100
+# }
+
+# # variable "object_array_with_type_and_default_1" {
+# #   type = list(object({
+# #               foo = string
+# #   }))
+# #   default = [{"foo"="bar"}]
+# # }
+
+# # variable "object_array_with_type_and_default_2" {
+# #   type = list(object({
+# #               foo = string
+# #   }))
+# #   default = [
+# #     {foo="bar"}
+# #   ]
+# # }
+
+# variable "object_with_type_and_default_1" {
+#   type = object({foo = string})
+#   default = {foo:"bar"}
+# }
 
 # variable "object_with_type_and_default_2" {
 #   type = object({
